c575afcb1af24dc217e1c5b271673268248536f7
alexander-nil <alexandern@live.se>
2024-02-11 22:57:47 +0100

    Added nextauth
    Added google oath client provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,23 @@
+"use client";
 import "./globals.css";
+import { SessionProvider, SessionProviderProps } from "next-auth/react";
 import StyledComponentsRegistry from "./lib/registry";
+import { Session } from "next-auth";
 
 export default function RootLayout({
 	children,
+	session,
 }: Readonly<{
 	children: React.ReactNode;
+	session: Session;
 }>) {
 	return (
-		<html lang="en">
-			<body>
-				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-			</body>
-		</html>
+		<SessionProvider session={session}>
+			<html lang="en">
+				<body>
+					<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+				</body>
+			</html>
+		</SessionProvider>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import { Button } from "@/components/button";
 import { Colors } from "./theme/colors";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
+	const user = useSession();
+
+	if (user.status === "unauthenticated") {
+		signIn();
+	}
 	return (
 		<main>
 			<Button color={Colors["Celtic Blue"]} onClick={() => console.log("ok")}>
