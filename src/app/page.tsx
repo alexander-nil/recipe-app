42c2eaa468e7b16c2fc44b8d6ee5f8b42ca8ff24
alexander nilsson <alexandern@live.se>
2024-02-11 16:50:44 +0100

    Add styled components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { Button } from "@/components/button";
+import { Colors } from "./theme/colors";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<Button color={Colors["Celtic Blue"]} onClick={() => console.log("ok")}>
+				Hello
+			</Button>
+		</main>
+	);
 }
---
+++ src/app/theme/colors/index.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+	"Celtic Blue": "#4472CA",
+	"Egyptian Blue": "#0A369D",
+	Glaucous: "#5E7CE2",
+	"Jordy Blue": "#92B4F4",
+	"Columbia Blue": "#CFDEE7",
+};
